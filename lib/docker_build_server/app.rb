6582c19aeaf8180b43855f4b74955d60194f309c
Dan Buch <d.buch@modcloth.com>
2013-11-05 16:00:09 -0500

    Block-i-fying all logger calls

--- lib/docker_build_server/app.rb
+++ lib/docker_build_server/app.rb
@@ -24,14 +24,15 @@ module DockerBuildServer
     set :travis_authenticators, Rack::Auth::Travis.default_authenticators
     set :travis_auth_disabled, !!ENV['DISABLE_TRAVIS_AUTH']
     enable :logging if ENV['ENABLE_LOGGING']
-
     set :log_level_string, (ENV['LOG_LEVEL'] || 'info').upcase
     set :log_level, Logger.const_get(settings.log_level_string)
 
     before do
       logger.level = settings.log_level if settings.logging?
-      logger.debug "log_level #{settings.log_level.inspect} " <<
-                   "(#{settings.log_level_string.inspect})"
+      logger.debug do
+        "log_level #{settings.log_level.inspect} " <<
+        "(#{settings.log_level_string.inspect})"
+      end
     end
 
     configure :development do
@@ -64,7 +65,7 @@ module DockerBuildServer
     post '/docker-build' do
       build_params = params.to_hash
       build_params = json_body if json?
-      logger.debug "build_params=#{build_params.inspect}"
+      logger.debug { "build_params=#{build_params.inspect}" }
 
       errors = validate_build_params(build_params)
       unless errors.empty?
@@ -84,14 +85,14 @@ module DockerBuildServer
       respond_to do |f|
         f.html do
           build_response = docker_build(build_params)
-          logger.debug "build_response=#{build_response.inspect}"
+          logger.debug { "build_response=#{build_response.inspect}" }
 
           session['flash'] = build_response['message']
           redirect 'index.html', 301
         end
         f.json do
           build_response = docker_build(build_params)
-          logger.debug "build_response=#{build_response.inspect}"
+          logger.debug { "build_response=#{build_response.inspect}" }
 
           status 201
           json build_response
@@ -100,23 +101,25 @@ module DockerBuildServer
     end
 
     post ENV['TRAVIS_WEBHOOK_PATH'] || '/travis-webhook' do
-      logger.debug %W(
-        authorization=#{request.env['HTTP_AUTHORIZATION'].inspect}
-        travis_repo_slug=#{request.env['HTTP_TRAVIS_REPO_SLUG'].inspect}
-        travis_authorized?=#{travis_authorized?.inspect}
-      ).join(', ')
+      logger.debug do
+        %W(
+          authorization=#{request.env['HTTP_AUTHORIZATION'].inspect}
+          travis_repo_slug=#{request.env['HTTP_TRAVIS_REPO_SLUG'].inspect}
+          travis_authorized?=#{travis_authorized?.inspect}
+        ).join(', ')
+      end
 
       halt 401 unless settings.travis_auth_disabled? || travis_authorized?
       halt 400 unless travis_payload
 
       halt 204 if travis_docker_build_disabled?
 
-      logger.debug "travis_payload=#{travis_payload.inspect}"
-      logger.debug "travis_build_params=#{travis_build_params.inspect}"
+      logger.debug { "travis_payload=#{travis_payload.inspect}" }
+      logger.debug { "travis_build_params=#{travis_build_params.inspect}" }
 
       build_response = docker_build(travis_build_params)
 
-      logger.debug "build_response=#{build_response.inspect}"
+      logger.debug { "build_response=#{build_response.inspect}" }
 
       status 201
       json build_response
